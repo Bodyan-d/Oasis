f4767e0aa57f967a9675a2e75fc6ed018e535494
Bohdan Diachenko <bogdandicenko315@gmail.com>
2024-06-01 13:27:25 +0200

    good-wheel

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -1,10 +1,14 @@
 import "./Main.css"
+import Spinwheel from "../Spinwheel/Spinwheel"
+
+
 
 function Main(){
     return(
         <div className="main-container">
             <div className="outer-blur"></div>
             <p className="attempts">ATTEMPTS: 2</p>
+            <Spinwheel/>
         </div>
         
     )
--- src/Components/Spinwheel/Spinwheel.js
+++ src/Components/Spinwheel/Spinwheel.js
@@ -1,5 +1,33 @@
+import "./Spinwheel.css"
+
 function Spinwheel() {
-    return;
+
+  const spin = ()=>{}
+  
+    return(
+      <div className="mainbox" id="mainbox">
+        <div className="box" id="box">
+          <div className="box1">
+            <span className="font span1"><p>something</p></span>
+            <span className="font span2"><p>something</p></span>
+            <span className="font span3"><p>something</p></span>
+            <span className="font span4"><p>something</p></span>
+            <span className="font span5"><p>something</p></span>
+            <span className="font span6"><p>something</p></span>
+          </div>
+          <div className="box2">
+            <span className="font span7"><p>something</p></span>
+            <span className="font span8"><p>something</p></span>
+            <span className="font span9"><p>something</p></span>
+            <span className="font span10"><p>something</p></span>
+            <span className="font span11"><p>something</p></span>
+            <span className="font span12"><p>something</p></span>
+          </div>
+          <button className="spin" onClick={spin}>SPIN</button></div>
+          
+      </div>
+    )
+    ;
 }
 
 export default Spinwheel
